816a6ccbf98490bdd34e094dbc7392d49fa2f16c
Nathan Loudon <vesnathan@gmail.com>
2025-01-28 10:11:55 +1100

    fixed schema for add client mutation

--- packages/cloudwatchlive/frontend/src/components/routes/clients/clientHooks.ts
+++ packages/cloudwatchlive/frontend/src/components/routes/clients/clientHooks.ts
@@ -1,22 +1,60 @@
+import {
+  saveClientMutationFn,
+  userQueryKeys,
+} from "@/graphql/queries/userQueries";
 import { useGraphqlMutation } from "@/hooks/useGraphQlMutation";
 import { CWLClient } from "shared/types/CWLClient";
 
-export const useSaveClientMutation = (input: { onSuccess: () => void }) => {
+export const useSaveClientMutation = (options?: {
+  onSuccess?: () => void;
+  getSuccessMessage?: () => string;
+  invalidate?: boolean;
+  additionalInvalidationKeys?: string[];
+}) => {
+  const {
+    onSuccess,
+    getSuccessMessage,
+    invalidate = true,
+    additionalInvalidationKeys = [],
+  } = options || {};
   return useGraphqlMutation({
-    invalidateKeys: [],
-    getSuccessMessage: () => {
-      return "Client data edited";
-    },
-    onSuccess: () => {
-      input.onSuccess();
-    },
-    mutationFn: async (_clientData: CWLClient) => {
-      const clientData: CWLClient = { ..._clientData };
-
-      await saveToDbMutation.mutateAsync({
-        fieldUpdates,
-        dataSet: "assets",
-        dataSource: "UserEdit",
+    onSuccess,
+    getSuccessMessage,
+    invalidateKeys: invalidate
+      ? [userQueryKeys.saveClient, ...additionalInvalidationKeys]
+      : [],
+    mutationFn: (input: CWLClient) => {
+      const {
+        id,
+        orgName,
+        createdDate,
+        createdBy,
+        addressLine1,
+        addressLine2,
+        city,
+        state,
+        country,
+        postalCode,
+        contactName,
+        contactEmail,
+        contactPhone,
+        contactRole,
+      } = input;
+      return saveClientMutationFn({
+        id,
+        orgName,
+        createdDate,
+        createdBy,
+        addressLine1,
+        addressLine2,
+        city,
+        state,
+        country,
+        postalCode,
+        contactName,
+        contactEmail,
+        contactPhone,
+        contactRole,
       });
     },
   });
