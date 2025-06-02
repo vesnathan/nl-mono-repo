555609d85098b9f7e8c9f159cbcab8ed5b47be5f
vesnathan <83148822+vesnathan@users.noreply.github.com>
2025-06-02 01:41:58 +0000

    Updates to deployment

--- packages/shared/functions/userGroup.ts
+++
@@ -1,7 +0,0 @@
-import type { AppSyncIdentityCognito } from "@aws-appsync/utils";
-import { ClientType } from "../../cloudwatchlive/backend/resources/AppSync/resolvers/gqlTypes";
-
-export const isSuperAdminUserGroup = (identity: AppSyncIdentityCognito): boolean => {
-  return (identity.groups || []).includes(ClientType.SuperAdmin);
-};
-
